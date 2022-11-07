b84abacb11258f15d1486d3b792cb151d21845d9
Justus Becker-Rabbenstein <justusbera@googlemail.com>
2022-11-07 17:09:29 +0100

    refactoring page switch logic

--- src/pages/bookmark/Bookmark.js
+++ src/pages/bookmark/Bookmark.js
@@ -5,17 +5,7 @@ import "./Bookmark.css";
 function Bookmark({ currentPage, setCurrentPage, cardsArray, varName }) {
   const [filteredArray, setFilteredArray] = useState([]);
 
-  function ffArray() {
-    /*         setFilteredArray(cardsArray.filter((p1) => cardsArray.isBookmarked === true)); */
-    console.log(varName);
-  }
-  return (
-    <>
-      <article className="card">
-        <button onClick={ffArray}>Test</button>
-      </article>
-    </>
-  );
+  return <Cards />;
 }
 
 export { Bookmark };
