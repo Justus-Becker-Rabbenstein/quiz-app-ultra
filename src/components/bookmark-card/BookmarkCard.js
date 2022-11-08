e7c6e64814fae6ce195c432e474344cc55324a89
Justus Becker-Rabbenstein <justusbera@googlemail.com>
2022-11-08 11:06:28 +0100

    refactoring card js and bookmarkcard js to use one less usestate vars

--- src/components/bookmark-card/BookmarkCard.js
+++ src/components/bookmark-card/BookmarkCard.js
@@ -1,43 +1,51 @@
-import React from 'react'
-import {ReactComponent as BookmarkLogoEmpty} from "../../assets/images/bookmark-regular.svg";
-import {ReactComponent as BookmarkLogoFull} from "../../assets/images/bookmark-solid.svg";
+import React from "react";
+import { ReactComponent as BookmarkLogoEmpty } from "../../assets/images/bookmark-regular.svg";
+import { ReactComponent as BookmarkLogoFull } from "../../assets/images/bookmark-solid.svg";
 import { useState } from "react";
 import "./BookmarkCard";
 
-const BookmarkCard = ({currentPage, setCurrentPage, cardsArray, setCardsArray, key}) => {
+const BookmarkCard = ({
+  currentPage,
+  setCurrentPage,
+  cardsArray,
+  setCardsArray,
+  key,
+}) => {
+  const [showAnswer, setShowAnswer] = useState(false);
 
-    const [showAnswer, setShowAnswer] = useState(false);
-    const [buttonString, setButtonString] = useState("Show answer");
-  
-    function buttonToggle() {
-      if (showAnswer === false) {
-        setShowAnswer(true);
-        setButtonString("Hide answer");
-      } else {
-        setShowAnswer(false);
-        setButtonString("Show answer");
-      }
+  function buttonToggle() {
+    if (showAnswer === false) {
+      setShowAnswer(true);
+    } else {
+      setShowAnswer(false);
     }
+  }
 
-    const result = cardsArray.filter((p1) => p1.isBookmarked);
+  const result = cardsArray.filter((p1) => p1.isBookmarked);
 
   return (
     <article className="card">
-    {console.log(result)}
-      {result.map((p1, p2) => 
-      <>
-      <section className="card__section">
-      {p1.isBookmarked ? <BookmarkLogoFull className="card__bookmark" /> : <BookmarkLogoEmpty className="card__bookmark"/>}
-      <h2 className="card__item">{p1.question}</h2>
-      <button className="card__button" onClick={buttonToggle}>
-        {`${buttonString}`}
-      </button>
-      <h3 className={showAnswer ? "card__item" : "card__item hide"}>{p1.answer}</h3>
-      </section>
-      </>
-      )}
+      {console.log(result)}
+      {result.map((p1, p2) => (
+        <>
+          <section className="card__section">
+            {p1.isBookmarked ? (
+              <BookmarkLogoFull className="card__bookmark" />
+            ) : (
+              <BookmarkLogoEmpty className="card__bookmark" />
+            )}
+            <h2 className="card__item">{p1.question}</h2>
+            <button className="card__button" onClick={buttonToggle}>
+              {showAnswer ? "Hide answer" : "Show answer"}
+            </button>
+            <h3 className={showAnswer ? "card__item" : "card__item hide"}>
+              {p1.answer}
+            </h3>
+          </section>
+        </>
+      ))}
     </article>
-  )
-}
+  );
+};
 
-export default BookmarkCard
+export default BookmarkCard;
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,36 +1,39 @@
 import { useState } from "react";
-import {ReactComponent as BookmarkLogoEmpty} from "../../assets/images/bookmark-regular.svg";
-import {ReactComponent as BookmarkLogoFull} from "../../assets/images/bookmark-solid.svg";
+import { ReactComponent as BookmarkLogoEmpty } from "../../assets/images/bookmark-regular.svg";
+import { ReactComponent as BookmarkLogoFull } from "../../assets/images/bookmark-solid.svg";
 import "./Card.css";
 
-function Card({ clickedBoolAnswer, cardsArray, setCardsArray, key}) {
+function Card({ clickedBoolAnswer, cardsArray, setCardsArray, key }) {
   const [showAnswer, setShowAnswer] = useState(false);
-  const [buttonString, setButtonString] = useState("Show answer");
 
   function buttonToggle() {
     if (showAnswer === false) {
       setShowAnswer(true);
-      setButtonString("Hide answer");
     } else {
       setShowAnswer(false);
-      setButtonString("Show answer");
     }
   }
 
   return (
     <article className="card">
-      {cardsArray.map((p1, p2) => 
-      <>
-      <section className="card__section">
-      {p1.isBookmarked ? <BookmarkLogoFull className="card__bookmark" /> : <BookmarkLogoEmpty className="card__bookmark"/>}
-      <h2 className="card__item">{p1.question}</h2>
-      <button className="card__button" onClick={buttonToggle}>
-        {`${buttonString}`}
-      </button>
-      <h3 className={showAnswer ? "card__item" : "card__item hide"}>{p1.answer}</h3>
-      </section>
-      </>
-      )}
+      {cardsArray.map((p1, p2) => (
+        <>
+          <section className="card__section">
+            {p1.isBookmarked ? (
+              <BookmarkLogoFull className="card__bookmark" />
+            ) : (
+              <BookmarkLogoEmpty className="card__bookmark" />
+            )}
+            <h2 className="card__item">{p1.question}</h2>
+            <button className="card__button" onClick={buttonToggle}>
+              {showAnswer ? "Hide answer" : "Show answer"}
+            </button>
+            <h3 className={showAnswer ? "card__item" : "card__item hide"}>
+              {p1.answer}
+            </h3>
+          </section>
+        </>
+      ))}
     </article>
   );
 }
