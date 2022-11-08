3acd60958429484283a39e96a37f9bb39f1ef4ed
Justus Becker-Rabbenstein <justusbera@googlemail.com>
2022-11-08 10:57:28 +0100

    refactoring of header and footer modules for better user expenrience

--- src/pages/cards/Cards.js
+++ src/pages/cards/Cards.js
@@ -3,14 +3,14 @@ import { Card } from "../../components/card/Card";
 import { Bookmark } from "../bookmark/Bookmark";
 import { useState } from "react";
 
-export function Cards({ currentPage, setCurrentPage, cardsArray, setCardsArray, key} ) {
-
-
+export function Cards({
+  currentPage,
+  setCurrentPage,
+  cardsArray,
+  setCardsArray,
+  key,
+}) {
   return (
-    <Card
-      cardsArray={cardsArray}
-      setCardsArray={setCardsArray}
-      key={key}
-    />
+    <Card cardsArray={cardsArray} setCardsArray={setCardsArray} key={key} />
   );
 }
