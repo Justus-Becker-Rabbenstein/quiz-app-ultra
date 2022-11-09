7e7a0e611309d21a2538e980d2008a0841294514
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2022-11-09 19:06:30 +0100

    added localStorage in Create js

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react";
 import "./Create.css";
 
 export function Create({ currentPage, setCurrentPage }) {
-  return <p>Profile / Create page is currently under construction</p>;
+  const text = 'Profile / Create page is currently under construction';
+  // setter
+  localStorage.setItem('uC', text);
+  // getter
+  const textFromStorage = localStorage.getItem('uC');
+
+
+  return <p>{textFromStorage}</p>;
 }
